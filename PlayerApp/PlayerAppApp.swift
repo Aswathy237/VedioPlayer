40afd5ae794b726da2d5f6ff02cd5655a1ff3aa5
Aswathy <aswathym237@gmail.com>
2025-02-16 17:20:50 +0530

    Added meta data

--- PlayerApp/PlayerAppApp.swift
+++ PlayerApp/PlayerAppApp.swift
@@ -11,7 +11,17 @@ import SwiftUI
 struct PlayerAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(video: Video(
+                title: "Sample Show",
+                episodeName: "Episode 1: The Beginning",
+                description: "This is a sample episode description.",
+                videoURL: Bundle.main.url(forResource: "BigBuckBunny", withExtension: "mp4"),
+                thumbnailURL: "https://media.istockphoto.com/id/1332455278/photo/smiling-white-light-brown-and-dark-brown-teddy-bears-sitting-on-table-at-pink-wall-background.jpg?s=612x612&w=0&k=20&c=vt3NvcLmXOFQQdbp_9_V3ZVlC1Dd47OFk4UEbgDd750=",
+                duration: 1200,
+                recommendations: [
+                    Video(title: "Sample Show 2", episodeName: "Episode 1", description: "Another sample.", videoURL: Bundle.main.url(forResource: "BigBuckBunny", withExtension: "mp4"), thumbnailURL: "", duration: 1200, recommendations: [])
+                ]
+            ))
         }
     }
 }
